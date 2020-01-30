efb83ffcca43e7c4b082f589bca89493841fc21d
Daniel Baquero <835@holbertonschool.com>
2020-01-30 00:01:53 +0000

    [Fixed]: Retrieves, creates, updates and deletes State Objects

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask import make_response
 
 
 app = Flask(__name__)
@@ -14,6 +15,12 @@ app.register_blueprint(app_views)
 def close_storage():
     """ Calls storage.close() method """
     storage.close()
+
+
+@app.errorhandler(404)
+def handling_error_404():
+    """ Handles error 404 """
+    return (make_response(jsonify({"error": "Not found"}), 404))
 
 if __name__ == "__main__":
     app.run(host=getenv("HBNB_API_HOST"), port=getenv("HBNB_API_PORT"),
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,22 @@
 """ Importing moduls¡es required for index """
 from api.v1.views import app_views
 from json import jsonify
+from models import storage
 
 
 @app.route('/status')
 def status_appviews():
     """ Checks app_view status """
     return (jsonify({"status": "OK"}))
+
+
+@app.route('/stats')
+def func_stats():
+    """
+    Creates and endpoint that retrieves the number of each objects by type
+    """
+    models = ['User', 'State', 'Review', 'Place', 'City', 'Amenity']
+    stats = {}
+    for item in models:
+        stats['item'] = storage.count(item)
+    return (jsonify(stats))
