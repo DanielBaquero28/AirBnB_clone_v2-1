40260594cc31ad178bbd2bcbe818cece14cc25d5
Skillhh <ortegon.giovanny@hotmail.com>
2020-01-30 22:15:45 -0500

    [FIX/UPDATE] Code close session and updating app.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,7 +8,7 @@ from models import storage
 @app_views.route('/status')
 def status():
     """ Checks app_view status """
-    return jsonify({'status':'OK'})
+    return jsonify({'status': 'OK'})
 
 
 @app_views.route('/stats')
